1a563db0e1157ecd9542ac2520ea32abc0c9b0a3
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-04 10:28:08 -0700

    Create as35.c

---
+++ as35.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+int a[i];
+printf("enter the digit");
+scanf("%d",a[i]);
+for(i=0;a[i]!='null';i++)
+{
+if(a[i]>='1'&&a[i]>='9')
+{
+printf("not special charater");
+}
+elseif(a[i]>='a'&&a[i]<='z')
+{
+printf("not special charater");
+}
+else
+{
+count++;
+}
+printf("%d",count);
+}
+}
