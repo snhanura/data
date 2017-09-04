2e87862ddfcc3c097c8f0dd62a70fbb9f99cb80a
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-04 10:20:23 -0700

    Create as34.c

---
+++ as34.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a[i];
+printf("enter the digit");
+scanf("%d",a[i]);
+for(i=0;a[i]!='null';i++)
+{
+if(a[i]>=1&&a[i]<=9)
+{
+count++;
+}
+printf("%d",count);
+}
+}
