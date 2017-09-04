2ae4570014875a5cb741344b7ed9d4db8453c109
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-04 10:03:41 -0700

    Create as31.c

---
+++ as31.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char a[i],i;
+int count=0;
+printf("enter the string value");
+scanf("%d",&a[i]);
+for(i=o;a[i]!='null';i++)
+{
+if(a[i]>'a'&&a[i]<'z')
+{
+count++;
+printf("%d",count);
+}
+}
+
