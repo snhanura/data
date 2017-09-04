d8911c779f3f184bc84393e4ad9ebfc65531273f
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-04 10:09:46 -0700

    Create as32.c

---
+++ as32.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char a[i],i;
+int count=0;
+printf("enter the string value");
+scanf("%d",&a[i]);
+for(i=o;a[i]!='null';i++)
+{
+if(a[i]=' ')
+{
+count++;
+}
+  printf("%d",count);
+}
